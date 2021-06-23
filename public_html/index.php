9d39d5bcfb87baa0d6db65c7b6337e3706873858
Vishal Maurya <vishalmaurya3112@gmail.com>
2021-06-23 13:01:45 +0530

    added navbar

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+    $title = 'Test Bootstrap';
+    require_once 'includes/header.php';
+    
+?>
+<?php
+    require_once 'includes/navbar.php';
+?>
+<?php
+    require_once 'includes/footer.php';
+?>
