81cb443613acdc58b79c9d09d0bb1edc3546b841
Vishal Maurya <vishalmaurya3112@gmail.com>
2021-07-12 12:34:39 +0530

    functionality for joining the event

---
+++ joiners/pages/joined_events.php
@@ -0,0 +1,12 @@
+<?php
+    include '../includes/joiners_header.php';
+    include '../includes/joiners_navbar.php';
+?>
+<div class="d-sm-flex align-items-center justify-content-between my-4">
+    <h1 class="h3 mb-0 text-gray-800">Joined Events</h1>
+</div>
+
+
+<?php
+    include '../includes/joiners_footer.php'
+?>
