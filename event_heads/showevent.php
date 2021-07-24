6080202284f26188ba37842f236244e501d7ac7c
Vishal Maurya <vishalmaurya3112@gmail.com>
2021-07-24 19:12:46 +0530

    Disabled the event name option in update event

--- event_heads/showevent.php
+++ event_heads/showevent.php
@@ -80,7 +80,7 @@ include 'includes/navbar.php';
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <label>Event Name</label>
-                                    <input type="text" class="form-control form-control-user" name="event_name" value="<?php echo $row['EVENT_NAME']; ?>" required>
+                                    <input type="text" class="form-control form-control-user disabled" name="event_name" value="<?php echo $row['EVENT_NAME']; ?>" disabled>
                                 </div>
                                 <div class="col-sm-6">
                                     <label>Sub event name</label>
